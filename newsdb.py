d3bd17e733c3a8f8a11c8fce65c05fa37135bec3
kvega <kevin.n.vega@gmail.com>
2018-05-28 14:24:18 -0700

    Setup initial scaffolding of newsdb

---
+++ newsdb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import psycopg2
+import bleach
+
+DBNAME = "news"
+
+def get_pop_articles():
+    """Returns the three articles with the highest number of views."""
+    pass
+
+
+def get_pop_authors():
+    """Return the three authors with the highest number of views."""
+    pass
+
+
+def get_error_days():
+    """Returns the dates of days on which more than 1% of requests lead to 
+    errors"""
+    pass
