865e9d2fd4d59eb3bd0b888e0b40e0206b7ff855
kvega <kevin.n.vega@gmail.com>
2018-06-01 00:07:42 -0700

    Removed the break from exit function

--- newsdb.py
+++ newsdb.py
@@ -17,9 +17,10 @@ def get_pop_articles():
         "limit 3;")
     top_articles = c.fetchall()
     db.close()
-
+    print("\n")
     for article in top_articles:
         print(article[0] + " ---- " + str(article[1]) + " views")
+    print("\n")    
     
 
 def get_pop_authors():
@@ -31,10 +32,10 @@ def get_pop_authors():
         "on authors.id = topauthors.author;")
     top_authors = c.fetchall()
     db.close()
-
+    print("\n")
     for author in top_authors:
         print(author[0] + " ---- " + str(author[1]) + " views")
-
+    print("\n")
 
 def get_error_days():
     """Returns the dates of days on which more than 1% of requests lead to 
@@ -48,31 +49,33 @@ def get_error_days():
         "where ((cast (failure as decimal) / success) * 100) > 1;")
     high_errors = c.fetchall()
     db.close()
-
+    print("\n")
     for error in high_errors:
         # Extract date to format it
         fdt = error[0].strftime('%B %d, %Y')
         print(fdt + ' ---- ' + str(round(error[3], 1)) + '%')
+    print("\n")        
 
 
-def __exit():
-	break
-	
-	
-if __name__ == '__main__':
-    print("Welcome to Log Analysis!")
-    opt = int(input("Please select one of the following options:\n" +
+def main():
+	print("Welcome to Log Analysis!\n")
+	while True:
+		opt = int(input("Please select one of the following options:\n" +
         " 1 - Get list of the top three most viewed articles\n" +
         " 2 - Get list of authors sorted by popularity\n" +
         " 3 - Check days on which more than 1% of requests lead to errors\n" +
         " 4 - Exit the program\n\n" +
-        "===>"))
+        "===> "))
 
-    if type(opt) != int or opt not in [1,2,3,4]:
-        print("Invalid option! Please enter a valid option.")
+		if type(opt) != int or opt not in [1,2,3,4]:
+			print("Invalid option! Please enter a valid option.")
     
-    options = {1: get_pop_articles,
-               2: get_pop_authors,
-               3: get_error_days,
-               4: __exit}
-    options[opt]()
+		options = {1: get_pop_articles,
+               	   2: get_pop_authors,
+               	   3: get_error_days}
+               
+		options[opt]()
+	
+	
+if __name__ == '__main__':
+    main()
